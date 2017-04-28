fbe6a2472a767be28fbec434695c61d1c7ed0b70
Zach <neunerzach@gmail.com>
2017-04-27 22:15:07 -0500

    waiting again

--- GameStateMachine.js
+++ GameStateMachine.js
@@ -150,7 +150,7 @@ gameManager.addEventListener(cast.receiver.games.EventType.GAME_MESSAGE_RECEIVED
 
 		if (k >= 2) {
             shuffle();
-            setTimeout(myFunction, 3000);
+            setTimeout(myFunction, 10000);
             gameManager.sendGameMessageToAllConnectedPlayers({ toDealScreen: "toDealState" });;
             gameData.phase = dealState;
             gameManager.updateGameData(gameData, false);
